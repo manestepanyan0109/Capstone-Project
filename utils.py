b4537d99f8787436ad945e4237ca26ebb0439ceb
manestepanyan0109 <manestepanyan0301@gmail.com>
2025-05-09 17:23:29 +0400

    updatw

---
+++ utils.py
@@ -0,0 +1,70 @@
+import numpy as np
+
+
+def s_entropy(freq_list):
+    freq_list = [element for element in freq_list if element != 0]
+    sh_entropy = 0.0
+    for freq in freq_list:
+        sh_entropy += freq * np.log(freq)
+    sh_entropy = -sh_entropy
+    return sh_entropy
+
+def _hash(x):
+    m, n = x.shape
+    if n == 1:
+        return np.zeros(m)
+    return np.sum(np.apply_along_axis(lambda y: y < x[:, 0], 0, x), axis=1) * np.math.factorial(n-1) + _hash(x[:, 1:])
+
+def ordinal_patterns(ts, embdim, embdelay):
+    m, t = embdim, embdelay
+    x = ts if isinstance(ts, np.ndarray) else np.array(ts)
+
+    tmp = np.zeros((x.shape[0], m))
+    for i in range(m):
+        tmp[:, i] = np.roll(x, i*t)
+    partition = tmp[(t*(m-1)):, :]
+    permutation = np.argsort(partition)
+    idx = _hash(permutation)
+
+    counts = np.zeros(np.math.factorial(m))
+    for i in range(counts.shape[0]):
+        counts[i] = (idx == i).sum()
+    return list(counts[counts != 0].astype(int))
+
+def p_entropy(op):
+    ordinal_pat = op
+    max_entropy = np.log(len(ordinal_pat))
+    p = np.divide(np.array(ordinal_pat), float(sum(ordinal_pat)))
+    return s_entropy(p) / max_entropy
+
+def complexity(op):
+    pe = p_entropy(op)
+    constant1 = (0.5 + ((1 - 0.5) / len(op))) * np.log(0.5 + ((1 - 0.5) / len(op)))
+    constant2 = ((1 - 0.5) / len(op)) * np.log((1 - 0.5) / len(op)) * (len(op) - 1)
+    constant3 = 0.5 * np.log(len(op))
+    Q_o = -1 / (constant1 + constant2 + constant3)
+
+    temp_op_prob = np.divide(op, sum(op))
+    temp_op_prob2 = (0.5 * temp_op_prob) + (0.5 * (1 / len(op)))
+    JSdivergence = (s_entropy(temp_op_prob2) - 0.5 * s_entropy(temp_op_prob) - 0.5 * np.log(len(op)))
+    Comp_JS = Q_o * JSdivergence * pe
+    return Comp_JS
+
+def weighted_ordinal_patterns(ts, embdim, embdelay):
+    m, t = embdim, embdelay
+    x = ts if isinstance(ts, np.ndarray) else np.array(ts)
+
+    tmp = np.zeros((x.shape[0], m))
+    for i in range(m):
+        tmp[:, i] = np.roll(x, i*t)
+    partition = tmp[(t*(m-1)):, :]
+    xm = np.mean(partition, axis=1)
+    weight = np.mean((partition - xm[:, None])**2, axis=1)
+    permutation = np.argsort(partition)
+    idx = _hash(permutation)
+    counts = np.zeros(np.math.factorial(m))
+    for i in range(counts.shape[0]):
+        counts[i] = sum(weight[i == idx])
+
+    return list(counts[counts != 0])
+
